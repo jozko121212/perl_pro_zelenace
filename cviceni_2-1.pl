cb5fdc0699cb5672df787dc66b34062386e20f1e
jozko121212 <jozef.mercak@gmail.com>
2015-10-19 19:14:09 +0200

    finished cviceni_2-1

--- cviceni_2-1.pl
+++ cviceni_2-1.pl
@@ -3,10 +3,26 @@
 use strict;
 use warnings;
 
+###  About $zisk variable
+print "multiplying: \n";
+#long version
 my $zisk = 20;
 $zisk = $zisk * 2;
-print "$zisk \n";
+print "from long version: $zisk \n";
 
+#shortened version
 my $zisk_skr = 20;
 $zisk_skr *= 2;
-print "$zisk_skr \n";
+print "from shortened version: $zisk_skr \n\n\n";
+
+###  About $sklad variable
+print "substracting: \n";
+#long version
+my $sklad = 20;
+my $prodano = 5;
+$sklad = $sklad - $prodano;
+print "from long version: $sklad\n";
+
+my $sklad_skr = 20;
+$sklad_skr -= $prodano;
+print "from shortened version: $sklad_skr\n\n\n";
