359fd13646d47361a3feffdb6e04a2018d55ed39
Jozef Mercak <jozer.mercak@gmail.com>
2016-01-08 15:40:30 +0100

    cviceni_6-8.pl

---
+++ cviceni_6-8.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+open(SUBOR1, "cviceni_6-8.txt") or die "Nemozem otvorit subor\n";
+my @ludia = ();
+while (my $riadok = <SUBOR1>) {
+	chomp($riadok);
+	$riadok =~ s/(\w*) (\w*)/$2 $1/gi;
+	push(@ludia,$riadok);
+}
+
+foreach my $i (sort(@ludia)) {print "$i\n";}
