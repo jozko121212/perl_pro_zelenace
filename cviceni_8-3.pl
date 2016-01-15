a719ab897f9f629aeeb750de987c8387d9d39742
jozko121212 <jozef.mercak@gmail.com>
2016-01-15 07:43:10 +0100

    cviceni_8-3.pl modified but still not working

--- cviceni_8-3.pl
+++ cviceni_8-3.pl
@@ -3,62 +3,78 @@
 use strict;
 use warnings;
 
-&inicializace;
-&vylosuj_cisla;
+my ($pocetcisel,$maxcislo,$uhodl,$poradi,$los);
+my ($cislo,$vylosovano) = (0,0);
+my %tazeno;
+my (@los,@prvni,@druzi,@treti);
+my $input_file = "cviceni_8-3.txt";
 
-while ( my @los = &dalsi_los ) {
-	my $poradi = &urci_poradi(@los);
-	if ( $poradi <= 3 ) {
-		&zarad_viteze($los[0],$poradi);
-	}
+open(LOSY,$input_file) or die "can't open input file: $input_file\n";
+
+&inicializace;#OK
+#&vylosuj_cisla;#OK
+
+&vypis_viteze;
+
+while (@los = &dalsi_los) {
+    $poradi = &urci_poradi(@los);
+	if ($poradi <= 3) {
+        &zarad_viteze($los[0],$poradi);
+    } 
 }
 
-srand;
-my $pocetcisel = 5; # globalni promenne
-my $maxcislo = 50;
+sub inicializace {
+	print "volana f-cia inicializace\n";
+	srand;
+	$pocetcisel = 5;
+	$maxcislo = 50;
+}
 
 sub vylosuj_cisla {
-	my ($cislo,$vylosovano) = (0,0);
-	my %tazeno;
-	while ( $vylosovano < $pocetcisel ) {
-		$cislo = int(rand($maxcislo)) + 1;
-		if ( not $tazeno{$cislo} ) {
-			$tazeno{$cislo} = 1;
+	print "volana f-cia vylosuj_cisla\n";
+	while ($vylosovano < $pocetcisel) {
+        $cislo = int(rand($maxcislo)) + 1;
+		if (not $tazeno{$cislo}) {
+            $tazeno{$cislo} = 1;
 			$vylosovano++;
-		}
-	}
-	print "Tazena byla cisla: ";
-	foreach $cislo ( sort {$a <=> $b} keys %tazeno ) {
-		print "$cislo ";
-	}
-	print "\n";
+        }
+        
+    }
+}
+
+foreach my $i (sort keys(%tazeno)) {
+	print "$i ... $tazeno{$i}\n";
 }
 
 sub dalsi_los {
-	open(LOSY,$ARGV[0]);
-	my $radek = <LOSY>;
-	chomp($radek);
-	return split(/\s+/,$radek);
+	print "volana f-cia dalsi_los\n";
+	my @radek = <LOSY>;
+	chomp(@radek);
+	#print "$radek\n";
+	return split(/\s+/,@radek);
 }
 
 sub urci_poradi {
-	my @los = @_;
-	my($cislo,$uhodl) = (0,0);
-	shift(@los);	# zbavim se cisla losu
+	print "volana f-cia urci_poradi\n";
+	@los = @_;
+	($cislo,$uhodl) = (0,0);
+	shift(@los);
 	foreach $cislo (@los) {
-		if ( $tazeno{$cislo} ) {$uhodl++;}
+		if ($tazeno{$cislo}) {$uhodl++;}
 	}
 	return $pocetcisel + 1 - $uhodl;
 }
 
 sub zarad_viteze {
-	my($los,$poradi) = @_;
-	if ( $poradi == 1 ) {push(@prvni,$los);}
-	elsif ( $poradi == 2 ) {push(@druzi,$los);}
-	elsif ( $poradi == 3 ) {push(@treti,$los);}
-}
+	print "volana f-cia zarad_viteze\n";
+	($los,$poradi) = @_;
+	if ($poradi == 1) {push(@prvni,$los);}
+	elsif ($poradi == 2) {push(@druzi,$los);}
+	elsif ($poradi == 3) {push(@treti,$los);}
+	}
 
-sub vypis_vyteze {
+sub vypis_viteze {
+	print "volana f-cia vypis_viteze\n";
 	print "Vyherci prvni ceny:\n";
 	&vypis_seznam(@prvni);
 	print "\nVyherci druhe ceny:\n";
@@ -68,7 +84,9 @@ sub vypis_vyteze {
 }
 
 sub vypis_seznam {
-	foreach my $los (sort { $a <=> $b } @_) {
+	print "volana f-cia vypis_seznam\n";
+	foreach $los (sort {$a <=> $b} @_) {
 		print "$los\n";
 	}
 }
+close(LOSY);
